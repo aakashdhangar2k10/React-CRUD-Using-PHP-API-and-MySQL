c3bcd7bf92afb4ea82f9dfb1fc11e28697bab64e
Akash Dhangar <aakashdhangar2k10@gmail.com>
2025-06-18 13:15:46 +0530

    User added using post method using php

--- src/Component/Adduser.js
+++ src/Component/Adduser.js
@@ -1,6 +1,31 @@
-import React from "react";
+import axios from "axios";
+import React, {useState} from "react";
+import { useNavigate } from 'react-router-dom';
 
-function Adduser() {
+
+function Adduser() 
+{
+    const navigate= useNavigate();
+    const [formvalue, setFormvalue]= useState({username:'',email:'',status:''});
+    const [message, setMessage]= useState('');
+    const handleInput = (e)=>{
+        setFormvalue({...formvalue, [e.target.name]:e.target.value});
+
+    }
+    const handleSubmit=async(e)=>{
+        e.preventDefault();
+        // console.log(formvalue);
+        const formData = {username:formvalue.username, email:formvalue.email, status:formvalue.status};
+        const res= await axios.post("http://localhost/reactcrudphp/api/user.php",formData); 
+        if(res.data.success){
+            setMessage(res.data.success);
+            setTimeout(() => {
+                navigate('/userlist');
+            }, 2000);
+        }
+    }
+
+
     return (
 
         <React.Fragment>
@@ -8,32 +33,33 @@ function Adduser() {
                 <div className="row">
                     <div className="col-md-6 mt-4">
                         <h5 className="mb-4">Adduser</h5>
-                        <form>
-                            <div class="mb-3 row">
-                                <label for="inputPassword" class="col-sm-2 ">Username</label>
-                                <div class="col-sm-10">
-                                    <input type="text" class="form-control" />
+                        <p className="text-warning"> { message }</p>
+                        <form onSubmit={handleSubmit}>
+                            <div className="mb-3 row">
+                                <label  className="col-sm-2 ">Username</label>
+                                <div className="col-sm-10">
+                                    <input type="text" name="username" value={formvalue.username} className="form-control" onChange={handleInput} />
                                 </div>
                             </div>
-                            <div class="mb-3 row">
-                                <label for="inputPassword" class="col-sm-2 ">Email</label>
-                                <div class="col-sm-10">
-                                    <input type="text" class="form-control"/>
+                            <div className="mb-3 row">
+                                <label  className="col-sm-2 ">Email</label>
+                                <div className="col-sm-10">
+                                    <input type="text" name="email"  value={formvalue.email} className="form-control" onChange={handleInput} />
                                 </div>   
                             </div>
-                            <div class="mb-3 row">
-                                <label for="inputPassword" class="col-sm-2 ">Status</label>
-                                <div class="col-sm-10">
-                                    <select name="status" className="form-control">
+                            <div className="mb-3 row">
+                                <label  className="col-sm-2 ">Status</label>
+                                <div className="col-sm-10">
+                                    <select name="status" value={formvalue.status} className="form-control" onChange={handleInput}>
                                         <option value="">-- Select Status --</option>
                                         <option value="1">Active</option>
-                                        <option value="1">Inactive</option>   
+                                        <option value="0">Inactive</option>   
                                     </select>
                                 </div>   
                             </div>
-                            <div class="mb-3 row">
-                                <label for="inputPassword" class="col-sm-2 "></label>
-                                <div class="col-sm-10">
+                            <div className="mb-3 row">
+                                <label  className="col-sm-2 "></label>
+                                <div className="col-sm-10">
                                     <button name="submit" className="btn btn-success">Submit</button>
                                 </div>   
                             </div>
--- src/Component/Userlist.js
+++ src/Component/Userlist.js
@@ -1,20 +1,19 @@
 import React from "react";
 
-function Userlist()
-{
-    return(
+function Userlist() {
+    return (
 
         <React.Fragment>
             <div className="container">
                 <div className="row">
                     <div className="col-md-6">
-                        <h5>Userlist</h5>                
+                        <h5>Userlist</h5>
                     </div>
-                    </div>
-                    </div>
-        
-    </React.Fragment>    
-    );  
+                </div>
+            </div>
+
+        </React.Fragment>
+    );
 }
 
 export default Userlist;
